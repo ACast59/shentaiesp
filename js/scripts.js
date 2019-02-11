339b847074722107b8fd537db952d1646f7a532f
Angel Castillo <32829371+ACast59@users.noreply.github.com>
2019-02-11 12:01:57 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,68 +9,6 @@ function navbarFunction() {
 
 $(document).ready(function() {
   $.ajax({
-    url: 'https://www.shentaiesp.asia/feeds/posts/default/-/Estrenos?alt=json-in-script&max-results=8',
-    type: 'get',
-    dataType: "jsonp",
-    success: function(data) {
-      var related_posturl, related_posttitle, related_postthumb, related_postthumbnail, related_html = '',
-      entry = data.feed.entry;
-      if (entry !== undefined) {
-        related_html = "";
-        for (var i = 0; i < entry.length; i++) {
-          for (var j=0; j < entry[i].link.length; j++) {
-            if (entry[i].link[j].rel == "alternate") {
-              related_posturl = entry[i].link[j].href;
-              break;
-            }
-          }
-          related_postthumb = entry[i].media$thumbnail.url;
-          related_postthumbnail = related_postthumb.replace("/s72-c", "/w350-h225-c");
-          related_posttitle = entry[i].title.$t;
-          related_html += '<div><a href="' + related_posturl + '" class="featured-carousel-anchor" title="' + related_posttitle + '"><img src="' + related_postthumbnail + '" alt="' + related_posttitle + '" /><div class="featured-carousel-caption">' + related_posttitle + '</div></a></div>';
-        }
-        related_html += '';
-        $('.featured-carousel').slick('slickAdd', related_html);
-      }
-    }
-  });
-  
-  $('.featured-carousel').slick({
-    autoplay: true,
-    autoplaySpeed: 5000,
-    infinite: true,
-    slidesToShow: 4,
-    arrows: false,
-    responsive:
-    [{
-      breakpoint: 960,
-      settings: {
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-      }
-    }]
-  });
-  
-  $('.featured-carousel-controls .carousel-arrow-left').click(function(e) {
-    $('.featured-carousel').slick('slickPrev');
-  });
-  
-  $('.featured-carousel-controls .carousel-arrow-right').click(function(e) {
-    $('.featured-carousel').slick('slickNext');
-  });
-  
-  $.ajax({
     url: 'https://www.shentaiesp.asia/feeds/posts/default/-/3D Motion?alt=json-in-script&max-results=6',
     type: 'get',
     dataType: "jsonp",
